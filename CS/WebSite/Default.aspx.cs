35175efa11b6e14e7bbc16312782826c5fa59873
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+using DevExpress.XtraScheduler;
+using DevExpress.Web.ASPxScheduler;
+
+public partial class _Default : System.Web.UI.Page {
+    private int lastInsertedAppointmentId;
+
+    protected void Page_Load(object sender, EventArgs e) {
+        ASPxScheduler1.DataBind();
+        if (!IsPostBack) {
+            if (ASPxScheduler1.Storage.Appointments.Count > 0)
+                ASPxScheduler1.Start = ASPxScheduler1.Storage.Appointments[0].Start;
+        }
+    }
+
+    protected void ASPxScheduler1_AppointmentRowInserting(object sender, DevExpress.Web.ASPxScheduler.ASPxSchedulerDataInsertingEventArgs e) {
+        e.NewValues.Remove("ID");
+    }
+
+    protected void SqlDataSourceAppointments_Inserted(object sender, SqlDataSourceStatusEventArgs e) {
+        SqlConnection connection = (SqlConnection)e.Command.Connection;
+
+        using (SqlCommand command = new SqlCommand("SELECT IDENT_CURRENT('CarScheduling')", connection)) {
+            lastInsertedAppointmentId = Convert.ToInt32(command.ExecuteScalar());
+        }
+    }
+
+    protected void ASPxScheduler1_AppointmentRowInserted(object sender, DevExpress.Web.ASPxScheduler.ASPxSchedulerDataInsertedEventArgs e) {
+        e.KeyFieldValue = lastInsertedAppointmentId;
+    }
+
+    protected void ASPxScheduler1_AppointmentsInserted(object sender, DevExpress.XtraScheduler.PersistentObjectsEventArgs e) {
+        Appointment apt = (Appointment)e.Objects[0];
+        ASPxSchedulerStorage storage = (ASPxSchedulerStorage)sender;
+        storage.SetAppointmentId(apt, lastInsertedAppointmentId);
+    }
+
+    protected void ASPxScheduler1_BeforeExecuteCallbackCommand(object sender, SchedulerCallbackCommandEventArgs e) {
+        if (e.CommandId == SchedulerCallbackCommandId.ClientSideUpdateAppointment)
+            e.Command = new CustomUpdateAppointmentCommand((ASPxScheduler)sender);
+        else if (e.CommandId == SchedulerCallbackCommandId.ClientSideInsertAppointment)
+            e.Command = new CustomInsertAppointmentCommand((ASPxScheduler)sender);
+    }
+}
---
+++ VB/WebSite/App_Code/CustomControllerAndCommands.vb
@@ -0,0 +1,85 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.XtraScheduler
+Imports DevExpress.Web.ASPxScheduler
+Imports DevExpress.Web.ASPxScheduler.Internal
+
+Public Class CustomInsertAppointmentCommand
+	Inherits AppointmentClientSideInsertCallbackCommand
+	Public Sub New(ByVal scheduler As ASPxScheduler)
+		MyBase.New(scheduler)
+	End Sub
+
+	Protected Overrides Function CreateAppointmentFormController(ByVal apt As Appointment) As AppointmentFormController
+		Return New CustomAppointmentFormController(Control, apt)
+	End Function
+
+	Protected Overrides Sub AssignControllerCustomFieldsValues(ByVal controller As AppointmentFormController, ByVal clientAppointment As ClientAppointmentProperties)
+		Dim editedAppointment As Appointment = controller.EditedAppointmentCopy
+
+		If clientAppointment.Properties("Price") IsNot Nothing Then
+			Dim result As Double = 0
+			Double.TryParse(clientAppointment.Properties("Price").ToString(), result)
+			editedAppointment.CustomFields("Price") = result
+		End If
+
+		MyBase.AssignControllerCustomFieldsValues(controller, clientAppointment)
+	End Sub
+End Class
+
+Public Class CustomUpdateAppointmentCommand
+	Inherits AppointmentClientSideUpdateCallbackCommand
+	Public Sub New(ByVal scheduler As ASPxScheduler)
+		MyBase.New(scheduler)
+	End Sub
+
+	Protected Overrides Function CreateAppointmentFormController(ByVal apt As Appointment) As AppointmentFormController
+		Return New CustomAppointmentFormController(Control, apt)
+	End Function
+
+	Protected Overrides Sub AssignControllerCustomFieldsValues(ByVal controller As AppointmentFormController, ByVal clientAppointment As ClientAppointmentProperties)
+		Dim editedAppointment As Appointment = controller.EditedAppointmentCopy
+
+		If clientAppointment.Properties("Price") IsNot Nothing Then
+			Dim result As Double = 0
+			Double.TryParse(clientAppointment.Properties("Price").ToString(), result)
+			editedAppointment.CustomFields("Price") = result
+		Else
+			editedAppointment.CustomFields("Price") = Nothing
+		End If
+
+		MyBase.AssignControllerCustomFieldsValues(controller, clientAppointment)
+	End Sub
+End Class
+
+Public Class CustomAppointmentFormController
+	Inherits AppointmentFormController
+	Private Const PriceFieldName As String = "Price"
+
+	Public Sub New(ByVal control As ASPxScheduler, ByVal apt As Appointment)
+		MyBase.New(control, apt)
+	End Sub
+
+	Public Property Price() As Nullable(Of Double)
+		Get
+			Return CType(EditedAppointmentCopy.CustomFields(PriceFieldName), Nullable(Of Double))
+		End Get
+		Set(ByVal value As Nullable(Of Double))
+			EditedAppointmentCopy.CustomFields(PriceFieldName) = value
+		End Set
+	End Property
+
+	Private Property SourcePrice() As Nullable(Of Double)
+		Get
+			Return CType(SourceAppointment.CustomFields(PriceFieldName), Nullable(Of Double))
+		End Get
+		Set(ByVal value As Nullable(Of Double))
+			SourceAppointment.CustomFields(PriceFieldName) = value
+		End Set
+	End Property
+
+
+	Protected Overrides Sub ApplyCustomFieldsValues()
+		SourcePrice = Price
+	End Sub
+End Class
